871d6d2728b3a905f9399138b1c6935aa8785e69
August Vaiksoo <august.vaiksoo@posteo.net>
2024-04-10 08:59:04 +0300

    Create initial form with reset & submit options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    ReactiveFormsModule, 
+    NgIf
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'test-work-form';
+  title = 'My Angular Form';
+
+  userForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    surname: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    workingExperience: new FormControl('', [Validators.required, Validators.pattern("^[0-9]*\\.?[0-9]+$")])
+  });
+
+  onSubmit() {
+    console.log(this.userForm.value);
+  }
+
+  resetForm() {
+    this.userForm.reset();
+  }
 }
